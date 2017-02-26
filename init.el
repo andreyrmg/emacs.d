8c8671d2e25a48babf07a742f2fe17896e7d36f6
Andrey Manzharov <andreyrmg@gmail.com>
2017-02-26 13:40:38 +0300

    показывать scratch buffer при старте emacs

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Initial-buffer-choice t)
  '(compilation-always-kill t)
  '(compilation-ask-about-save nil)
  '(compilation-auto-jump-to-first-error nil)
